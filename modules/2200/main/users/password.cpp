7b79c18c0e9a28527aacf5771f186a226e283734
Graham Cox <graham@grahamcox.co.uk>
2015-05-26 19:34:52 +0100

    Support for generating a UUID for the salt, and an SHA256 for the hash

--- modules/2200/main/users/password.cpp
+++ modules/2200/main/users/password.cpp
@@ -1,11 +1,35 @@
 #include "password.h"
+#include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_generators.hpp>
+#include <boost/uuid/uuid_io.hpp>
+#include <openssl/sha.h>
+
+/** The UUID Generator to use */
+static boost::uuids::random_generator uuidGenerator;
 
 namespace Users {
-    Password Password::hash(const std::string& password) {
-        return Password::hash(password, "salt");
-    }
+  Password Password::hash(const std::string& password) {
+    boost::uuids::uuid uuid = uuidGenerator();
+    
+    return Password::hash(password, boost::uuids::to_string(uuid));
+  }
+  
+  Password Password::hash(const std::string& password, const std::string& salt) {
+    unsigned char hash[SHA256_DIGEST_LENGTH];
 
-    Password Password::hash(const std::string& password, const std::string& salt) {
-        return Password(password, salt);
+    SHA256_CTX sha256;
+    SHA256_Init(&sha256);
+    SHA256_Update(&sha256, salt.c_str(), salt.size());
+    SHA256_Update(&sha256, password.c_str(), password.size());
+    SHA256_Final(hash, &sha256);
+
+    std::string hashedPassword;
+    char bytePair[2];
+    for (unsigned char h : hash) {
+      sprintf(bytePair, "%02x", h);
+      hashedPassword += bytePair;
     }
+    
+    return Password(hashedPassword, salt);
+  }
 }
--- modules/2200/test/users/passwordTest.cpp
+++ modules/2200/test/users/passwordTest.cpp
@@ -1,28 +1,30 @@
 #include "test.h"
 #include "users/password.h"
 #include <regex>
+/** Regular Expression to match the Salt, which is a UUID */
+static const auto SALT_REGEX = std::regex("^[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}$");
 
-static const auto UUID_REGEX = std::regex("^[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}$");
+/** Regular Expression to match the Hash, which is an SHA256 hash as hex pairs  */
+static const auto HASH_REGEX = std::regex("^[a-z0-9]{64}$");
 
 const lest::test module[] = {
-    SCENARIO("Hashing a password") {
-        GIVEN("A Plaintext password") {
-            const std::string plaintext = "password";
-
-            WHEN("Hashed with a random salt") {
-                const Users::Password password = Users::Password::hash(plaintext);
-
-                THEN("The salt is a UUID") {
-                    EXPECT(std::regex_match(password.salt(), UUID_REGEX));
-                }
-
-                THEN("The hash is an SHA256 string") {
-                }
-            }
-
-
+  SCENARIO("Hashing a password") {
+    GIVEN("A Plaintext password") {
+      const std::string plaintext = "password";
+      
+      WHEN("Hashed with a random salt") {
+        const Users::Password password = Users::Password::hash(plaintext);
+        
+        THEN("The salt is a UUID") {
+          EXPECT(std::regex_match(password.salt(), SALT_REGEX));
         }
+        
+        THEN("The hash is an SHA256 string") {
+          EXPECT(std::regex_match(password.hash(), HASH_REGEX));
+        }
+      }  
     }
+  }
 };
 
 MODULE(specification(), module);
