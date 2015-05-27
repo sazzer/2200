90e0ca95f693008e73ff6a9c17b3a6d4de81773c
Graham Cox <graham@grahamcox.co.uk>
2015-05-27 07:10:03 +0100

    Replaced Lest with Catch (sorry)

--- modules/2200/test/users/passwordTest.cpp
+++ modules/2200/test/users/passwordTest.cpp
@@ -1,4 +1,4 @@
-#include "test.h"
+#include "catch.hpp"
 #include "users/password.h"
 #include <regex>
 /** Regular Expression to match the Salt, which is a UUID */
@@ -7,63 +7,29 @@ static const auto SALT_REGEX = std::regex("^[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-
 /** Regular Expression to match the Hash, which is an SHA256 hash as hex pairs  */
 static const auto HASH_REGEX = std::regex("^[a-z0-9]{64}$");
 
-const lest::test module[] = {
-  SCENARIO("Hashing a password") {
-    GIVEN("A Plaintext password") {
-      const std::string plaintext = "password";
-      
-      WHEN("Hashed with a random salt") {
-        const Users::Password password = Users::Password::hash(plaintext);
-        
-        THEN("The salt is a UUID") {
-          EXPECT(std::regex_match(password.salt(), SALT_REGEX));
-        }
-        
-        THEN("The hash is an SHA256 string") {
-          EXPECT(std::regex_match(password.hash(), HASH_REGEX));
-        }
-      }  
-    }
-  },
+TEST_CASE("Hashing a password", "[password]") {
+  const Users::Password password = Users::Password::hash("password");
 
-  SCENARIO("Comparing Passwords") {
-    GIVEN("A hashed password") {
-      const Users::Password password = Users::Password::hash("password", "salt");
-      
-      WHEN("Comparing to another hashed password") {
-        THEN("The same password is a match") {
-          EXPECT(Users::Password::hash("password", "salt") == password);
-        }
-        
-        THEN("A different salt is not a match") {
-          EXPECT_NOT(Users::Password::hash("password", "different") == password);
-        }
-        
-        THEN("A different password is not a match") {
-          EXPECT_NOT(Users::Password::hash("different", "salt") == password);
-        }
-      }
-      
-      WHEN("Comparing to a string") {
-        THEN("The same password is a match") {
-          EXPECT(password == "password");
-        }
-        THEN("A different password is a match") {
-          EXPECT_NOT(password == "different");
-        }
-      }
-      
-      WHEN("Comparing a string to it") {
-        THEN("The same password is a match") {
-          EXPECT("password" == password);
-        }
-        THEN("A different password is a match") {
-          EXPECT_NOT("different" == password);
-        }
-      }
-      
-    }
+  CHECK(std::regex_match(password.salt(), SALT_REGEX));
+  CHECK(std::regex_match(password.hash(), HASH_REGEX));
+}
+
+TEST_CASE("Comparing Passwords", "[password]") {
+  const Users::Password password = Users::Password::hash("password", "salt");
+  
+  SECTION("Comparing to another hashed password") {
+    CHECK(Users::Password::hash("password", "salt") == password);
+    CHECK_FALSE(Users::Password::hash("password", "different") == password);
+    CHECK_FALSE(Users::Password::hash("different", "salt") == password);
   }
-};
 
-MODULE(specification(), module);
+  SECTION("Comparing to a string") {
+    CHECK(password == "password");
+    CHECK_FALSE(password == "different");
+  }
+
+  SECTION("Comparing a string to it") {
+    CHECK("password" == password);
+    CHECK_FALSE("different" == password);
+  }
+}
--- modules/shared/test/main.cpp
+++ modules/shared/test/main.cpp
@@ -1,16 +1,2 @@
-#include "lest.hpp"
-
-lest::tests& specification() {
-    static lest::tests tests;
-    return tests;
-}
-
-/**
- * Main runner for the tests
- * @param argc The number of arguments
- * @param argv The actual arguments
- */
-int main( int argc, char * argv[] )
-{
-    return lest::run( specification(), argc, argv );
-}
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
