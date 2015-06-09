498bff09163decc8d821f171d87e5e355afd4c62
Graham Cox <graham@grahamcox.co.uk>
2015-06-09 07:08:39 +0100

    Some tidying up

--- src/main/frontend/GruntFile.js
+++ src/main/frontend/GruntFile.js
@@ -1,42 +1,33 @@
-module.exports = function(grunt){
-    require('time-grunt')(grunt);
-    require('jit-grunt')(grunt, {
-        'bower': 'grunt-bower-task'
+module.exports = function(grunt) {
+    require("time-grunt")(grunt);
+    require("jit-grunt")(grunt, {
+        bower: "grunt-bower-task"
     });
-
-    var webpack = require('webpack');
-    var webpackConfig = require('./webpack.config.js');
-
-   // configure tasks
-   grunt.initConfig({
-      bower: {
-        build: {
-            options: {
-                targetDir: '${project.build.outputDirectory}/external',
-                install: true,
-                cleanup: false,
-                copy: true,
-                layout: 'byType',
-                verbose: true
+    var webpack = require("webpack");
+    var webpackConfig = require("./webpack.config.js");
+    grunt.initConfig({
+        bower: {
+            build: {
+                options: {
+                    targetDir: "${project.build.outputDirectory}/external",
+                    install: true,
+                    cleanup: false,
+                    copy: true,
+                    layout: "byType",
+                    verbose: true
+                }
+            }
+        },
+        webpack: {
+            options: webpackConfig,
+            build: {
+                plugins: webpackConfig.plugins.concat(new webpack.DefinePlugin({
+                    "process.env": {
+                        NODE_ENV: JSON.stringify("production")
+                    }
+                }), new webpack.optimize.DedupePlugin())
             }
         }
-      },
-      webpack: {
-        options: webpackConfig,
-        build: {
-            plugins: webpackConfig.plugins.concat(
-                         new webpack.DefinePlugin({
-                             'process.env': {
-                                'NODE_ENV': JSON.stringify('production')
-                             }
-                         }),
-                         new webpack.optimize.DedupePlugin()
-                     )
-        }
-      }
-   });
-
-   grunt.registerTask('default', ['bower', 'webpack']);
-
+    });
+    grunt.registerTask("default", [ "bower", "webpack" ]);
 };
-
--- src/main/frontend/webpack.config.js
+++ src/main/frontend/webpack.config.js
@@ -1,47 +1,32 @@
-var webpack = require('webpack');
+var webpack = require("webpack");
 
 module.exports = {
     cache: true,
-    entry: '${project.basedir}/src/main/javascript/main.js',
+    entry: "${project.basedir}/src/main/javascript/main.js",
     output: {
-        path: '${project.build.outputDirectory}/javascript',
-        filename: '[name].bundle.js'
+        path: "${project.build.outputDirectory}/javascript",
+        filename: "[name].bundle.js"
     },
     module: {
-        loaders: [
-            { test: /\.js$/, loaders: [
-                '${project.build.directory}/frontend/node_modules/babel-loader'
-            ]}
-        ]
+        loaders: [ {
+            test: /\.js$/,
+            loaders: [ "${project.build.directory}/frontend/node_modules/babel-loader" ]
+        } ]
     },
     resolve: {
-        root: [
-            '${project.build.directory}/frontend/bower_components'
-        ],
-        extensions: [
-            '', 
-            '.js',
-            '.jsx',
-            '.json'
-        ],
-        alias: {
-        }
+        root: [ "${project.build.directory}/frontend/bower_components" ],
+        extensions: [ "", ".js", ".jsx", ".json" ],
+        alias: {}
     },
     stats: {
         colors: true,
         modules: true,
         errors: true
     },
-    plugins: [
-        new webpack.dependencies.LabeledModulesPlugin(),
-        new webpack.ProvidePlugin({
-        }),
-        new webpack.ResolverPlugin(
-            new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin('bower.json', ['main'])
-        ),
-        new webpack.DefinePlugin({
-            VERSION: JSON.stringify('${project.version}')
-        })
+    plugins: [ 
+        new webpack.dependencies.LabeledModulesPlugin(), 
+        new webpack.ProvidePlugin({}), 
+        new webpack.ResolverPlugin(new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin("bower.json", [ "main" ])), 
+        new webpack.DefinePlugin({ VERSION: JSON.stringify("${project.version}") }) 
     ]
 };
-
