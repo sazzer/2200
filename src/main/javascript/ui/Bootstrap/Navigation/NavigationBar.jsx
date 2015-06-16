bcf7d970a26856d395c1f51d19fcd6b46601ea59
Graham Cox <graham@grahamcox.co.uk>
2015-06-16 12:34:43 +0100

    Made the navbar fluid so it uses the fulls creen

--- src/main/javascript/ui/Bootstrap/Navigation/NavigationBar.jsx
+++ src/main/javascript/ui/Bootstrap/Navigation/NavigationBar.jsx
@@ -24,7 +24,7 @@ export const NavigationBar = React.createClass({
             .reverse();
 
         return <nav className="navbar navbar-default navbar-static-top">
-            <div className="container">
+            <div className="container-fluid">
                 <div className="navbar-header">
                     <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
                         <span className="sr-only">{this.getIntlMessage("Bootstrap.NavigationBar.toggle")}</span>
