f80e3cea85b616b1a921985f8e1fcb1212fa2deb
Graham Cox <graham@grahamcox.co.uk>
2015-06-16 08:20:43 +0100

    Login form started

--- src/main/javascript/ui/home/LoginForm.jsx
+++ src/main/javascript/ui/home/LoginForm.jsx
@@ -9,6 +9,19 @@ export const LoginForm = React.createClass({
      * @return {ReactElement} The React representation of this element
      */
     render: function() {
-        return <div>Login Form</div>;
+        return <form>
+            <div className="form-group">
+                <label for="username">Email Address</label>
+                <input type="email" className="form-control" name="username" placeholder="Enter Email Address"/>
+            </div>
+            <div className="form-group">
+                <label for="password">Password</label>
+                <input type="password" className="form-control" name="password" placeholder="Enter Password"/>
+            </div>
+            <div className="">
+                <button type="submit" className="btn btn-primary">Log In</button>
+                <button type="submit" className="btn">Register</button>
+            </div>
+        </form>;
     }
 });
