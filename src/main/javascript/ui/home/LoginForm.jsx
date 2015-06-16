4b637bf9911260295798250e6e23d3215cb4846e
Graham Cox <graham@grahamcox.co.uk>
2015-06-16 12:32:52 +0100

    Strings for the login form, and a single large login button

--- src/main/javascript/ui/home/LoginForm.jsx
+++ src/main/javascript/ui/home/LoginForm.jsx
@@ -1,27 +1,26 @@
 import React from "react";
+import ReactIntl from "react-intl";
 
 /**
  * The login form for when the page first loads
  */
 export const LoginForm = React.createClass({
+    mixins: [ReactIntl.IntlMixin],
     /**
      * Actually render the login form
      * @return {ReactElement} The React representation of this element
      */
     render: function() {
-        return <form>
+        return <div>
             <div className="form-group">
-                <label for="username">Email Address</label>
-                <input type="email" className="form-control" name="username" placeholder="Enter Email Address"/>
+                <label for="username">{this.getIntlMessage("page.LoginForm.username.label")}</label>
+                <input type="email" className="form-control" name="username" placeholder={this.getIntlMessage("page.LoginForm.username.placeholder")}/>
             </div>
             <div className="form-group">
-                <label for="password">Password</label>
-                <input type="password" className="form-control" name="password" placeholder="Enter Password"/>
+                <label for="password">{this.getIntlMessage("page.LoginForm.password.label")}</label>
+                <input type="password" className="form-control" name="password" placeholder={this.getIntlMessage("page.LoginForm.password.placeholder")}/>
             </div>
-            <div className="">
-                <button type="submit" className="btn btn-primary">Log In</button>
-                <button type="submit" className="btn">Register</button>
-            </div>
-        </form>;
+            <button type="submit" className="btn btn-lg btn-primary btn-block">{this.getIntlMessage("page.LoginForm.buttons.login")}</button>
+          </div>;
     }
 });
